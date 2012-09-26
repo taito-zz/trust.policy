971fdedd7cee6864b8cfd3f11426b59ba8353a1b
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-27 01:10:24 +0300

    Setuphandler removes front-page. Added viewlets.xml.

--- README.rst
+++ README.rst
@@ -5,6 +5,12 @@ trust.policy
 Change log
 ----------
 
+0.2 (2012-09-XX)
+================
+
+- Setuphandler removes front-page. [taito]
+- Added viewlets.xml. [taito]
+
 0.1 (2012-09-21)
 ================
 
--- trust/policy/setuphandlers.py
+++ trust/policy/setuphandlers.py
@@ -10,15 +10,20 @@ logger = logging.getLogger(__name__)
 def uninstall_package(context, packages):
     """Uninstall packages.
 
-:param packages: List of package names.
-:type packages: list
-"""
+    :param packages: List of package names.
+    :type packages: list
+    """
     portal = context.getSite()
     installer = getToolByName(portal, 'portal_quickinstaller')
     packages = [
         package for package in packages if installer.isProductInstalled(package)
     ]
     installer.uninstallProducts(packages)
+
+def remove_front_page(context):
+    portal = context.getSite()
+    if portal.get('front-page'):
+        portal.manage_delObjects(['front-page'])
 
 
 def setupVarious(context):
@@ -28,3 +33,4 @@ def setupVarious(context):
 
     uninstall_package(context, ['plonetheme.classic'])
     ISecuritySchema(context.getSite()).set_enable_user_folders(True)
+    remove_front_page(context)
